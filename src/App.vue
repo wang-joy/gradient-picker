5de16c45344004212c87a95ab8a57ac55efeac17
wang-joy <wang_joy13@163.com>
2018-12-11 14:11:50 +0800

    重新创建项目

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <button @click="dialogFormVisible=true">弹出</button>
+    <gradient-picker-conatiner></gradient-picker-conatiner>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import GradientPickerConatiner from './gradient-picker/PickerContainer'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    GradientPickerConatiner
+  },
+  data () {
+    return {
+      dialogFormVisible: false,
+      dialogWidth: '400px'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
